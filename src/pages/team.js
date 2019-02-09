7859e711bed16f3a38b045e03658970a3bb0de68
Christian Alloway <christianalloway@crescentschool.org>
2019-02-09 14:36:22 -0500

    added TeamPageHeader Component, Modified css to make it stick(not working yet)

--- src/pages/team.js
+++ src/pages/team.js
@@ -5,9 +5,20 @@ class Team extends Component {
         return (
             <div>
                 <p>Requested team: {this.props.match.params.teamNum}</p>
+                <TeamPageHeader num={this.props.match.params.teamNum} />
             </div>
         );
     }
+}
+
+class TeamPageHeader extends Component {
+  render() {
+    return (
+      <div class="header sticky" id="myHeader">
+        <h2>Team #{this.props.num}</h2>
+      </div>
+    );
+  }
 }
 
 export default Team;
