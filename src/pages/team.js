d4f787691f493c33d2d463cbc7fa1e66e0201c04
Jax <jaxhodgkinson@crescentschool.org>
2019-02-11 15:54:31 -0500

    Added side bar to team page

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
-class Team extends Component {
+class Sidebar extends Component {
     render() {
         return (
             <div>
-                <p>Requested team: {this.props.match.params.teamNum}</p>
+                <li>
+                    <div id="addButtonDiv"></div>
+                    <div id="remButtonDiv"></div>
+                </li>
+                <li class="active">
+                    <p>Team Name:</p>
+                    <input type="number" id="teamName" min="1" max="99999"></input>
+                </li>
+                <li>
+                    <table>
+                        <tr>
+                            <th><a href='#overall'>Overall</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#cycles'>Cycles</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#pregame'>Pregame</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#endgame'>Endgame</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#misc'>Misc</a></th>
+                        </tr>
+                    </table>
+                </li>
             </div>
         );
     }
