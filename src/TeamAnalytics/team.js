d0f966d4cb122dcb5c8fe6cecc62e50097d82f7f
markhydroxyl <markhydroxyl@gmail.com>
2019-02-12 18:09:10 -0500

    Refactored naming; refactored CSS classNames

--- src/TeamAnalytics/team.js
+++ src/TeamAnalytics/team.js
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
+import TeamAnalyticsHeader from './TeamAnalyticsHeader/TeamAnalyticsHeader';
 
 class Team extends Component {
-    render() {
-        return (
-            <div>
-                <p>Requested team: {this.props.match.params.teamNum}</p>
-                <TeamPageHeader num={this.props.match.params.teamNum} />
-            </div>
-        );
-    }
-}
-
-class TeamPageHeader extends Component {
-  componentDidMount() {
-   window.addEventListener('scroll', this.handleScroll);
- }
- componentWillUnmount() {
-   window.removeEventListener('scroll', this.handleScroll);
- }
- handleScroll = () => {
-   let header = document.getElementById("teamPageHeader");
-   let belowTop = header.offsetTop;
-   if (window.pageYOffset > belowTop) {
-     header.classList.add("sticky");
- } else {
-   header.classList.remove("sticky");
-   }
-}
-
-  render() {
-    return (
-      <div className="header" id="teamPageHeader">
-        <h2>Team #{this.props.num}</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<p>Requested team: {this.props.match.params.teamNum}</p>
+				<TeamAnalyticsHeader num={this.props.match.params.teamNum} />
+			</div>
+		);
+	}
 }
 
 export default Team;
