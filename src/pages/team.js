3b58213ccb3a899bf32a95932e048cba179e7b9b
TimothyW553 <31230953+TimothyW553@users.noreply.github.com>
2019-02-11 19:12:48 -0500

    Collapsible Banners

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import Accordion from './Accordion';
 
 class Team extends Component {
-    render() {
-        return (
-            <div>
-                <p>Requested team: {this.props.match.params.teamNum}</p>
-            </div>
-        );
-    }
+  render() {
+    return(
+      <div>
+      <h1>Robot Information</h1>
+      <Accordion allowMultipleOpen>
+        <div label='Overall' isOpen>
+          <p><strong>Common Name:</strong> American Alligator</p>
+          <p><strong>Distribution:</strong> Texas to North Carolina, US</p>
+          <p><strong>Endangered Status:</strong> Currently Not Endangered</p>
+        </div>
+        <div label='Cycles'>
+          <p><strong>Common Name:</strong> Chinese Alligator</p>
+          <p><strong>Distribution:</strong> Eastern China</p>
+          <p><strong>Endangered Status:</strong> Critically Endangered</p>
+        </div>
+        <div label='Pregame'>
+          <p><strong>Common Name:</strong> Chinese Alligator</p>
+          <p><strong>Distribution:</strong> Eastern China</p>
+          <p><strong>Endangered Status:</strong> Critically Endangered</p>
+        </div>
+        <div label='Endgame'>
+          <p><strong>Common Name:</strong> Chinese Alligator</p>
+          <p><strong>Distribution:</strong> Eastern China</p>
+          <p><strong>Endangered Status:</strong> Critically Endangered</p>
+        </div>
+        <div label='Miscellaneous'>
+          <p><strong>Common Name:</strong> Chinese Alligator</p>
+          <p><strong>Distribution:</strong> Eastern China</p>
+          <p><strong>Endangered Status:</strong> Critically Endangered</p>
+        </div>
+        <div label='Comments'>
+          <p><strong>Common Name:</strong> Chinese Alligator</p>
+          <p><strong>Distribution:</strong> Eastern China</p>
+          <p><strong>Endangered Status:</strong> Critically Endangered</p>
+        </div>
+      </Accordion>
+    </div>
+    );
+  }
 }
 
 export default Team;
