1cc13217bf58031682b99ad29b92381f91b8b7ac
Timothy Wang <timothy.art@gmail.com>
2019-02-13 18:10:10 -0500

    added collapsible labels

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import Overall from "./Overview/Overall";
 import Cycles from "./Overview/Cycles";
@@ -6,16 +5,19 @@ import Endgame from "./Overview/Endgame";
 import Misc from "./Overview/misc";
 import header from "./Overview/TeamHeader";
 import TeamHeader from "./Overview/TeamHeader";
+import Accordion from './Overview/Accordion';
 
 class Team extends Component {
   render() {
     return (
       <div>
         <TeamHeader num={this.props.match.params.teamNum} />
-        <Overall num={this.props.match.params.teamNum} />
-        <Cycles />
-        <Endgame />
-        <Misc />
+      <Accordion allowMultipleOpen>
+        <div label = "Overall" isOpen><Overall num={this.props.match.params.teamNum} /></div>
+        <div label = "Cycles" isOpen><Cycles /></div>
+        <div label = "Endgame" isOpen><Endgame /></div>
+        <div label = "Miscellaneous" isOpen><Misc /></div>
+      </Accordion>
       </div>
     );
   }
