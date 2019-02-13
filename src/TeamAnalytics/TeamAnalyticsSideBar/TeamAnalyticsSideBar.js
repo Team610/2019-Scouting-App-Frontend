4b20d3d59acb1af73528a53309f5819a7f95eed6
Felix Yu <markhydroxyl@gmail.com>
2019-02-12 19:40:08 -0500

    Moved Sidebar component to separate folder and file

---
+++ src/TeamAnalytics/TeamAnalyticsSideBar/TeamAnalyticsSideBar.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+class TeamAnalyticsSideBar extends Component {
+    render() {
+        return (
+            <div>
+                <li>
+                    <div id="addButtonDiv"></div>
+                    <div id="remButtonDiv"></div>
+                </li>
+                <li class="active">
+                    <p>Team Name:</p>
+                    <input type="number" id="teamName" min="1" max="99999"></input>
+                </li>
+                <li>
+                    <table>
+                        <tr>
+                            <th><a href='#overall'>Overall</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#cycles'>Cycles</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#pregame'>Pregame</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#endgame'>Endgame</a></th>
+                        </tr>
+                        <tr>
+                            <th><a href='#misc'>Misc</a></th>
+                        </tr>
+                    </table>
+                </li>
+            </div>
+        );
+    }
+}
+
+export default TeamAnalyticsSideBar;
--- src/TeamAnalytics/team.js
+++ src/TeamAnalytics/team.js
@@ -1,45 +1,10 @@
 import React, { Component } from 'react';
+import TeamAnalyticsSideBar from './TeamAnalyticsSideBar/TeamAnalyticsSideBar';
 
 class Team extends Component {
     render() {
-        return (<Sidebar />);
+        return (<TeamAnalyticsSideBar />);
     }
 }
 
-class Sidebar extends Component {
-    render() {
-        return (
-            <div>
-                <li>
-                    <div id="addButtonDiv"></div>
-                    <div id="remButtonDiv"></div>
-                </li>
-                <li class="active">
-                    <p>Team Name:</p>
-                    <input type="number" id="teamName" min="1" max="99999"></input>
-                </li>
-                <li>
-                    <table>
-                        <tr>
-                            <th><a href='#overall'>Overall</a></th>
-                        </tr>
-                        <tr>
-                            <th><a href='#cycles'>Cycles</a></th>
-                        </tr>
-                        <tr>
-                            <th><a href='#pregame'>Pregame</a></th>
-                        </tr>
-                        <tr>
-                            <th><a href='#endgame'>Endgame</a></th>
-                        </tr>
-                        <tr>
-                            <th><a href='#misc'>Misc</a></th>
-                        </tr>
-                    </table>
-                </li>
-            </div>
-        );
-    }
-}
-
-export default Sidebar;
+export default Team;
