77cd2b478ac04e0d45e395876b31333ea36ff8bb
Christian Alloway <christianalloway@crescentschool.org>
2019-02-15 14:42:47 -0500

    Top Banner Team Number

---
+++ src/MatchForm/InMatchForm/DefenseInput/DefenseInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class DefenseInput extends Component {
+    getJSON() {
+        return "";
+    }
+    render() {
+        return(<p> Not Done Yet </p>);
+    }
+}
+
+export default DefenseInput;
---
+++ src/TeamPage/teamPageHeader.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class TeamPageHeader extends Component {
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+    handleScroll = () => {
+        let header = document.getElementById("teamPageHeader");
+        let belowTop = header.offsetTop;
+        if (window.pageYOffset > belowTop) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+
+    render() {
+        return (
+            <div className="stickyHeader" id="teamPageHeader">
+                <h2>Team #{this.props.teamNum}</h2>
+            </div>
+        );
+    }
+}
+
+export default TeamPageHeader;
