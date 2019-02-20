446d63eefed03bfdc0fa0b8367cbacc43f43f997
Timothy Wang <timothy.art@gmail.com>
2019-02-19 19:07:51 -0500

    Added ChartJS-2 (React)

--- src/TeamAnalytics/team.js
+++ src/TeamAnalytics/team.js
@@ -6,16 +6,51 @@ import PreGameSection from './PreGameSection/preGameSection';
 import EndGameSection from './EndGameSection/endGameSection';
 import DefenseSection from './DefenseSection/defenseSection';
 import CommentsSection from './CommentsSection/commentsSection';
+import Chart from './ChartJS/Chart';
 import './style.css';
 
 class Team extends Component {
+
 	constructor(props) {
 		super(props);
 		this.state = {
-			dataAvailable:false
+			dataAvailable:false,
+			charData:{}
 		}
 		this.teamNum = this.props.match.params.teamNum
 	}
+
+	componentWillMount(){
+		this.getChartData();
+	}
+
+	getChartData(){
+		let txt = '{"33":{"1":{"def_tough_defense":[],"robot_preload":["cargo"],"cargo_lv3":[],"def_driving_around":[],"def_ship_goalkeep":[],"start_on_lvl_2":["true"],"comments":["undefined"],"climb_lvl":["1"],"hatch_lv1":[],"hatch_lv2":[1.104],"hatch_lvS":[],"climb_time":["0"],"hatch_lv3":[],"cargo_lvS":[],"def_rocket_goalkeep":[],"ship_preloads":["cargo","hatch"],"cargo_lv2":[],"cargo_lv1":[],"def_pinning":[1.096]}}}';
+		let obj = JSON.parse(txt);
+		console.log(obj[33]);
+		this.setState({
+      chartData:{
+        labels: [],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
 	async componentWillMount() {
 		let data = await fetch(`/api/v1/stats/team/${this.teamNum}/agg`);
 		data = await data.json();
@@ -26,6 +61,13 @@ class Team extends Component {
 	}
     render() {
 		console.log(this.state.data);
+		return (
+      <div className="App">
+        <div className="App-header">
+        </div>
+        <Chart chartData={this.state.chartData} location="33" legendPosition="bottom"/>
+      </div>
+    );
 		if(!this.state.dataAvailable) {
 			return(
 				<div>
@@ -34,8 +76,8 @@ class Team extends Component {
 				</div>
 			);
 		}
-        return (
-            <div>
+    return (
+      <div>
 				<TeamAnalyticsHeader teamNum={this.teamNum} />
 				<OverallSection data={this.state.data} />
 				<CyclesSection data={this.state.data} />
@@ -43,9 +85,9 @@ class Team extends Component {
 				<EndGameSection data={this.state.data} />
 				<DefenseSection data={this.state.data} />
 				<CommentsSection />
-            </div>
-        );
-    }
+      </div>
+    );
+	}
 }
 
 export default Team;
