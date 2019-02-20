3f16fdfcbb51006f48617cde5f60b668364517e9
Christian Alloway <christianalloway@crescentschool.org>
2019-02-20 09:32:25 -0500

    Fixed quick button error

--- src/MatchForm/InMatchForm/DefenseInput/DefenseInput.js
+++ src/MatchForm/InMatchForm/DefenseInput/DefenseInput.js
@@ -4,7 +4,7 @@ import Modal from "react-responsive-modal";
 
 class DefenseInput extends Component {
   getJSON() {
-      return {defType:this.defType, duration:(this.endTime - this.startTime)/1000};
+    return {defType:this.defType, duration:(this.endTime - this.startTime)/1000};
   }
 
   constructor(props) {
@@ -38,19 +38,19 @@ class DefenseInput extends Component {
     this.defType = type;
   }
 
-    render() {
+  render() {
     const { open } = this.state;
     return (
       <div>
-        <button onClick={this.onOpenModal}>Defense</button>
-        <Modal open={open} onClose={this.onCloseModal} center>
-        <br/>
-        <button style={{margin:5}} onClick={()=>{this.onCloseModal('rocket_goalkeep')}}> Rocket Goalkeep </button><br/>
-        <button style={{margin:5}} onClick={()=>{this.onCloseModal('ship_goalkeep')}}> Ship Goalkeep </button><br/>
-        <button style={{margin:5}} onClick={()=>{this.onCloseModal('pinning')}}> Pinning </button><br/>
-        <button style={{margin:5}} onClick={()=>{this.onCloseModal('driving_around')}}> Driving Around </button><br/>
-        <button style={{margin:5}} onClick={()=>{this.onCloseModal('tough_defense')}}> Tough Defense </button><br/>
-        </Modal>
+      <button onClick={this.onOpenModal}>Defense</button>
+      <Modal open={open} onClose={this.onCloseModal} center>
+      <br/>
+      <button style={{margin:5}} onClick={()=>{this.onCloseModal('rocket_goalkeep')}}> Rocket Goalkeep </button><br/>
+      <button style={{margin:5}} onClick={()=>{this.onCloseModal('ship_goalkeep')}}> Ship Goalkeep </button><br/>
+      <button style={{margin:5}} onClick={()=>{this.onCloseModal('pinning')}}> Pinning </button><br/>
+      <button style={{margin:5}} onClick={()=>{this.onCloseModal('driving_around')}}> Driving Around </button><br/>
+      <button style={{margin:5}} onClick={()=>{this.onCloseModal('tough_defense')}}> Tough Defense </button><br/>
+      </Modal>
       </div>
     );
   }
