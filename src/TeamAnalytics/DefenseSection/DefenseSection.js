cb5aa5b6c538e36e44de46967026e6f9570910e4
Felix Yu <markhydroxyl@gmail.com>
2019-03-02 16:53:48 -0500

    adding previously non-existent defense analytics

--- src/TeamAnalytics/DefenseSection/DefenseSection.js
+++ src/TeamAnalytics/DefenseSection/DefenseSection.js
@@ -27,33 +27,33 @@ class DefenseSection extends Component {
 					<tbody>
 						<tr>
 							<td className="analyticsTable">Total</td>
-							<td className="analyticsTable">{this.props.data.tot_time_def}</td>
-							<td className="analyticsTable" />
+							<td className="analyticsTable">{this.props.data.tot_time_def}s</td>
+							<td className="analyticsTable">{this.props.data.tot_matches_def}</td>
 						</tr>
 						<tr>
 							<td className="analyticsTable">Ship Goalkeeping</td>
-							<td className="analyticsTable">{this.props.data.tot_time_def_ship_goalkeep}</td>
-							<td className="analyticsTable" />
+							<td className="analyticsTable">{this.props.data.tot_time_def_ship_goalkeep}s</td>
+							<td className="analyticsTable">{this.props.data.tot_matches_def_ship_goalkeep}</td>
 						</tr>
 						<tr>
 							<td className="analyticsTable">Rocket Goalkeeping</td>
-							<td className="analyticsTable">{this.props.data.tot_time_def_rocket_goalkeep}</td>
-							<td className="analyticsTable" />
+							<td className="analyticsTable">{this.props.data.tot_time_def_rocket_goalkeep}s</td>
+							<td className="analyticsTable">{this.props.data.tot_matches_def_rocket_goalkeep}</td>
 						</tr>
 						<tr>
 							<td className="analyticsTable">Pinning</td>
-							<td className="analyticsTable">{this.props.data.tot_time_def_pinning}</td>
-							<td className="analyticsTable" />
+							<td className="analyticsTable">{this.props.data.tot_time_def_pinning}s</td>
+							<td className="analyticsTable">{this.props.data.tot_matches_def_pinning}</td>
 						</tr>
 						<tr>
 							<td className="analyticsTable">Tough Defense</td>
-							<td className="analyticsTable">{this.props.data.tot_time_def_tough_defense}</td>
-							<td className="analyticsTable" />
+							<td className="analyticsTable">{this.props.data.tot_time_def_tough_defense}s</td>
+							<td className="analyticsTable">{this.props.data.tot_matches_def_tough_defense}</td>
 						</tr>
 						<tr>
 							<td className="analyticsTable">Driving Around</td>
-							<td className="analyticsTable">{this.props.data.tot_time_def_driving_around}</td>
-							<td className="analyticsTable" />
+							<td className="analyticsTable">{this.props.data.tot_time_def_driving_around}s</td>
+							<td className="analyticsTable">{this.props.data.tot_matches_def_driving_around}</td>
 						</tr>
 						<tr>
 							<td className="analyticsTable" />
