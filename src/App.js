fdd4a23b8ce22fb6b476d1ee1e2c472ca3707843
justinmetivier <justinmetivier@crescentschool.org>
2019-03-01 23:22:27 -0500

    Added camera and photo taking capacilites, on submit -> creates js object with orientation, team number, and base64 Image

--- src/App.js
+++ src/App.js
@@ -2,87 +2,146 @@ import React from "react";
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 // import SampleImportPage from "./pages/samplePage";
 import Loadable from "react-loadable";
-import './App.css'; //TODO: make CSS modularized
+import "./App.css"; //TODO: make CSS modularized
 
 const App = () => (
-    <Router>
-        <div>
-            <Header />
-            <Route exact path="/" component={Home} />
-            <Route path="/form" component={MatchForms} />
-            <Route path="/teams" component={Teams} />
-            <Route path="/overall" component={OverallTable} />
-        </div>
-    </Router>
+  <Router>
+    <div>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/form" component={MatchForms} />
+      <Route path="/teams" component={Teams} />
+      <Route path="/overall" component={OverallTable} />
+      <Route path="/robotPhotos" component={RobotPhotos} />
+    </div>
+  </Router>
 );
 //TODO: figure out how to implement 404 page <Route component={NoMatch} />
 
 const Home = () => <h2>Home</h2>;
-const Loading = (props) => <div>Loading {props.page}...</div>;
+const Loading = props => <div>Loading {props.page}...</div>;
 
 const MatchForm = Loadable({
-    loader: () => import("./MatchForm/MatchForm"),
-    loading: Loading
+  loader: () => import("./MatchForm/MatchForm"),
+  loading: Loading
 });
 const MatchForms = ({ match }) => (
-    <div>
-        <h2>Forms</h2>
-        <ul>
-            <li><NavLink to={`${match.url}/1`}>Match 1</NavLink></li>
-            <li><NavLink to={`${match.url}/2`}>Match 2</NavLink></li>
-            <li><NavLink to={`${match.url}/3`}>Match 3</NavLink></li>
-			<li><NavLink to={`${match.url}/4`}>Match 4</NavLink></li>
-            <li><NavLink to={`${match.url}/5`}>Match 5</NavLink></li>
-            <li><NavLink to={`${match.url}/6`}>Match 6</NavLink></li>
-			<li><NavLink to={`${match.url}/7`}>Match 7</NavLink></li>
-			<li><NavLink to={`${match.url}/8`}>Match 8</NavLink></li>
-			<li><NavLink to={`${match.url}/9`}>Match 9</NavLink></li>
-			<li><NavLink to={`${match.url}/10`}>Match 10</NavLink></li>
-			<li><NavLink to={`${match.url}/11`}>Match 11</NavLink></li>
-			<li><NavLink to={`${match.url}/12`}>Match 12</NavLink></li>
-			<li><NavLink to={`${match.url}/13`}>Match 13</NavLink></li>
-        </ul>
+  <div>
+    <h2>Forms</h2>
+    <ul>
+      <li>
+        <NavLink to={`${match.url}/1`}>Match 1</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/2`}>Match 2</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/3`}>Match 3</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/4`}>Match 4</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/5`}>Match 5</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/6`}>Match 6</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/7`}>Match 7</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/8`}>Match 8</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/9`}>Match 9</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/10`}>Match 10</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/11`}>Match 11</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/12`}>Match 12</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/13`}>Match 13</NavLink>
+      </li>
+    </ul>
 
-        <Route path={`${match.path}/:matchId`} render={
-            (props) => <MatchForm {...props}/>} />
-        <Route exact path={match.path} render={
-            () => <p>Please select a match number.</p>} />
-    </div>
+    <Route
+      path={`${match.path}/:matchId`}
+      render={props => <MatchForm {...props} />}
+    />
+    <Route
+      exact
+      path={match.path}
+      render={() => <p>Please select a match number.</p>}
+    />
+  </div>
 );
 
 const Team = Loadable({
-    loader: () => import("./TeamAnalytics/TeamAnalytics"),
-    loading: Loading
+  loader: () => import("./TeamAnalytics/TeamAnalytics"),
+  loading: Loading
 });
 const Teams = ({ match }) => (
-    <div>
-        <h2>Teams</h2>
-        <ul>
-            <li><NavLink to={`${match.url}/610`}>610</NavLink></li>
-            <li><NavLink to={`${match.url}/33`}>33</NavLink></li>
-        </ul>
+  <div>
+    <h2>Teams</h2>
+    <ul>
+      <li>
+        <NavLink to={`${match.url}/610`}>610</NavLink>
+      </li>
+      <li>
+        <NavLink to={`${match.url}/33`}>33</NavLink>
+      </li>
+    </ul>
 
-        <Route path={`${match.path}/:teamNum`} render={
-            (props) => <Team {...props} />} />
-        <Route exact path={match.path} render={
-            () => <p>Please select a team.</p>} />
-    </div>
+    <Route
+      path={`${match.path}/:teamNum`}
+      render={props => <Team {...props} />}
+    />
+    <Route
+      exact
+      path={match.path}
+      render={() => <p>Please select a team.</p>}
+    />
+  </div>
 );
 
 const OverallTable = Loadable({
-    loader: () => import("./OverallTable/SortableTable"),
-    loading: Loading
+  loader: () => import("./OverallTable/SortableTable"),
+  loading: Loading
+});
+
+const RobotPhotos = Loadable({
+  loader: () => import("./RobotPhotos/Camera/Photo"),
+  loading: Loading
 });
 
 const Header = () => (
-    <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/form">Form</NavLink></li>
-        <li><NavLink to="/teams">Teams</NavLink></li>
-        <li><NavLink to="/sample">Sample</NavLink></li>
-        <li><NavLink to="/overall">Overall</NavLink></li>
-    </ul>
+  <ul>
+    <li>
+      <NavLink to="/">Home</NavLink>
+    </li>
+    <li>
+      <NavLink to="/form">Form</NavLink>
+    </li>
+    <li>
+      <NavLink to="/teams">Teams</NavLink>
+    </li>
+    <li>
+      <NavLink to="/sample">Sample</NavLink>
+    </li>
+    <li>
+      <NavLink to="/overall">Overall</NavLink>
+    </li>
+    <li>
+      <NavLink to="/robotPhotos">Robot Photos</NavLink>
+    </li>
+  </ul>
 );
-const NoMatch = () => (<div>404 Error: Page not found</div>);
+const NoMatch = () => <div>404 Error: Page not found</div>;
 
 export default App;
---
+++ src/RobotPhotos/Camera/review.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
+import "./robotPhoto.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+
+class Review extends Component {
+  constructor(props) {
+    super(props);
+    this.teamNumber = this.props.teamNum;
+    this.orientation = this.props.orient;
+    console.log(this.props.img);
+    this.image = this.props.img;
+  }
+
+  render() {
+    console.log(this.teamNumber);
+    return (
+      <div>
+        <img src={this.image} />
+      </div>
+    );
+  }
+}
+
+export default Review;
